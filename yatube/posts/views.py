f435539a76f125be05f1206473c77df0102aca89
Alim Gubzhokov <gubzhal@yandex.ru>
2021-12-07 15:27:20 +0300

    Delete comments from views.py

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -5,8 +5,10 @@ from django.http import HttpResponse
 from .models import Post, Group
 
 # Create your views here.
+
+
 def index(request):
-    
+
     posts = Post.objects.order_by('-pub_date')[:10]
     context = {
         'posts': posts,
@@ -16,12 +18,10 @@ def index(request):
 
 
 def group_posts(request, slug):
-   
+
     group = get_object_or_404(Group, slug=slug)
-    # posts = Post.objects.filter(group=group).order_by('-pub_date')[:-10]
-    posts = group.posts.all().order_by("-pub_date")
+    posts = group.posts.all().order_by("-pub_date")[:10]
     context = {
-        # 'slug': slug,
         'group': group,
         'posts': posts,
     }
